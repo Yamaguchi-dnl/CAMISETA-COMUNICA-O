3fe45ca8fe80a9373a1bcacc64456b1f91fa2717
Daniel Vieira <danielyamaguchi400@gmail.com>
2026-03-09 16:50:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-276890851-91158",
+  "appId": "1:533662681774:web:4f1055d1d71a16f04cbb3f",
+  "apiKey": "AIzaSyBi_P-ZKlHxotacIK0Hnii9jPLNfLkS96s",
+  "authDomain": "studio-276890851-91158.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "533662681774"
+};
