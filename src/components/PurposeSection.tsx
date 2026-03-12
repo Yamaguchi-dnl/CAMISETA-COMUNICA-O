d1b7ba003925ae8d608840c601c8ee46296d96e1
Daniel Vieira <danielyamaguchi400@gmail.com>
2026-03-12 16:31:22 +0000

    DESLOQUE ESSA IMAGEM DA SEÇÃO DE PROPOSITO DO CASAL MAIS 20PX PARA BAIXO

--- src/components/PurposeSection.tsx
+++ src/components/PurposeSection.tsx
@@ -84,8 +84,8 @@ export function PurposeSection() {
             {/* Losango Decorativo (Diamond) */}
             <div className="purpose-diamond absolute top-[50%] left-[48%] -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-[#d81f32] rotate-45 z-[4] shadow-md" />
 
-            {/* Imagem Secundária (Menor, sobreposta) - Deslocada 50px para baixo */}
-            <div className="purpose-image-secondary absolute bottom-[-50px] right-0 w-[68%] md:w-[360px] aspect-square z-[3] overflow-hidden">
+            {/* Imagem Secundária (Menor, sobreposta) - Deslocada 70px para baixo no total */}
+            <div className="purpose-image-secondary absolute bottom-[-70px] right-0 w-[68%] md:w-[360px] aspect-square z-[3] overflow-hidden">
               <Image 
                 src="https://ik.imagekit.io/q0yw2qaik/Camiseta%20IAP%20BARREIRINHA/PEDRO%20E%20SARA%20-%20COSTAS%20E%20FRENTE.jpg" 
                 alt="Propósito IAP Kit Camisetas" 
